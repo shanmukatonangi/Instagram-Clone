29f221d2f2dbacd7069dd329b7986534f535bb5e
shanmukatonangi <shanmukatonangi@gmail.com>
2025-11-20 18:39:25 +0530

    20Nov-login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
-      <Signup />
+      {/* <Signup /> */}
+      <Login />
     </div>
   )
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./login.css"
+
+const Login = () => {
+  return (
+    <div id="login">
+        <div id='banner'></div>
+        <div id='details-login'>
+            <div id='facebook'>
+                <img src='src/assets/OIP.jpeg' style={{height:"70px"}} />
+               
+               
+            </div>
+            <div id='user-details-login'>
+                <input type="text" placeholder='mobile number or email or username' />
+                <input type="text" placeholder='Password' />
+                
+                
+                <button>Login</button>
+            </div>
+        <div id='login-det'>
+            <p>Have an account ?</p>
+            <p>Login</p>
+        </div>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Login
